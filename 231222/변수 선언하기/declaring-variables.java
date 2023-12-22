1fcccf82afc65e88e6bf50947d66008b8073bbbb
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-22 17:58:50 +0900

    Title: 변수 선언하기, Time: 89ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231222/변수 선언하기/declaring-variables.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 3;
+        char c = 'C';
+        System.out.printf("%d\n%s", a, c);
+    }
+}
