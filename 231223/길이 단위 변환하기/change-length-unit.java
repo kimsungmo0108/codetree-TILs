b547abad6c18baa38f27e0cbff36e2b809af4144
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-23 13:02:47 +0900

    Title: 길이 단위 변환하기, Time: 106ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231223/길이 단위 변환하기/change-length-unit.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        float a = 9.2f;
+        float b = 1.3f;
+        float c = a*30.48f;
+        float d = b*160934;
+        System.out.printf("%.1fft = %.1fcm\n", a, c);
+        System.out.printf("%.1fmi = %.1fcm", b, d);
+    }
+}
