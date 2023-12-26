37afdd8ae5116a489fe17ea7bc2d66f00b94fcf7
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-26 12:42:36 +0900

    Title: 합을 복사하기, Time: 94ms, Memory: 8MB, Status: Passed - CodeTree

---
+++ 231226/합을 복사하기/copy-the-sum.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        int a = 1, b = 2, c = 3;
+        int t = a + b + c;
+        a = b = c = t;
+        System.out.printf("%d %d %d", a, b, c);
+    }
+}
