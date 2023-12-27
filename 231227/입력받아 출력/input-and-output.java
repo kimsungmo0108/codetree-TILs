a24d80c7a504bfd997e7af59de67dec21f9c8413
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-27 09:15:02 +0900

    Title: 입력받아 출력, Time: 140ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231227/입력받아 출력/input-and-output.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d %d", a, b);
+    }
+}
