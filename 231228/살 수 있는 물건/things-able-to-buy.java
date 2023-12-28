ba28594b2ca02b9fa16f82c1fa4a9a0a4c25bee6
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-28 17:12:00 +0900

    Title: 살 수 있는 물건, Time: 118ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 231228/살 수 있는 물건/things-able-to-buy.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        switch(a){
+            case 3000:{
+                System.out.println("book");
+                break;
+            }
+            case 1000:{
+                System.out.println("mask");
+                break;
+            }
+        }
+
+    }
+}
