e2c0e09c7db26b703aac456f573f0c3d36a45767
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-28 17:28:43 +0900

    Title: 살 수 있는 물건 2, Time: 137ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231228/살 수 있는 물건 2/things-able-to-buy-2.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>=3000){
+            System.out.println("book");
+        }else if(a>=1000&&a<3000){
+            System.out.println("mask");
+        }else if(a>=500&&a<1000){
+            System.out.println("pen");
+        }else {System.out.println("no");}
+    }
+}
