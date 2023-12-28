e31a6ef665d9a7bf1a7d1ae32b308636b502d6e1
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-28 09:29:37 +0900

    Title: 숫자의 조건 여부, Time: 157ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231228/숫자의 조건 여부/number's-condition.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        if(a>=113){
+            System.out.println("1");
+        }else{
+            System.out.println("0");
+        }
+    }
+}
