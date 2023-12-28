d24551f07a328253cea430caa1fd9c2134aa53d9
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-28 17:03:27 +0900

    Title: 삼항연산자, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231228/삼항연산자/ternary-operator.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int score = sc.nextInt();
+        System.out.println(score == 100 ? "pass" : "failure");
+    }
+}
