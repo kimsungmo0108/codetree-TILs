966862eb5a2c38ca776cc594a652ef85f3e945c4
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-29 15:11:14 +0900

    Title: 3 또는 5의 배수, Time: 126ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231229/3 또는 5의 배수/multiples-of-3-or-5.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        if(a%3 == 0){
+            System.out.println("YES");
+        }else{System.out.println("NO");}
+        if(a%5 == 0){
+            System.out.println("YES");
+        }else{System.out.println("NO");}
+    }
+}
