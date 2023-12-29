c4b5eba9a94a76ace33363229eb741fdfedb257c
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-29 17:34:52 +0900

    Title: 점수 비교, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231229/점수 비교/score-comparison.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int d = sc.nextInt();
+
+        if(a > c && b > d){System.out.println(1);
+        } else{System.out.println(0);}
+    }
+}
