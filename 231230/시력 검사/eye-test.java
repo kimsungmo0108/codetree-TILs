05a43571f5acd2f1d2b43c3aa17819847c9ded62
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-30 12:41:28 +0900

    Title: 시력 검사, Time: 258ms, Memory: 11MB, Status: Passed - CodeTree

--- 231230/시력 검사/eye-test.java
+++ 231230/시력 검사/eye-test.java
@@ -3,9 +3,10 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         java.util.Scanner sc = new java.util.Scanner(System.in);
         float a = sc.nextFloat();
-        if(a>=1.0){
+        float b = sc.nextFloat();
+        if(a>=1.0 && b>=1.0){
             System.out.println("High");
-        }else if(a>=0.5 && a<1.0){
+        }else if(a>=0.5 && b>=0.5){
             System.out.println("Middle");
             }else{System.out.println("Low");}
     }
