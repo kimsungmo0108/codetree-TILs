2af0d08eab14b73d629f4123683cd5e85e5b5ef5
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2023-12-31 18:02:06 +0900

    Title: 세 정수의 최솟값, Time: 119ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 231231/세 정수의 최솟값/minimum-of-three-numbers.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int min = a<b?a:b;
+        min = min<c?min:c;
+        System.out.println(min);
+    }
+}
