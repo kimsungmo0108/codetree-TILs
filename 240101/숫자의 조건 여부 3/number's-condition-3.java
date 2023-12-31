00d0dd1c9d3c8efb89fee60474f0df306a510ec8
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-01-01 04:11:26 +0900

    Title: 숫자의 조건 여부 3, Time: 141ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240101/숫자의 조건 여부 3/number's-condition-3.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        if(a%13 == 0 || a%19 == 0 ){
+            System.out.println("True");
+        } else{
+            System.out.println("False");
+        }
+        
+    }
+}
