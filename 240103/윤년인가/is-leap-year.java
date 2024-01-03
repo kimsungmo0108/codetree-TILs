77d6e743d59182fb0b5e14d0333ecde082c87819
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-01-03 18:11:53 +0900

    Title: 윤년인가, Time: 130ms, Memory: 10MB, Status: Passed - CodeTree

--- 240103/윤년인가/is-leap-year.java
+++ 240103/윤년인가/is-leap-year.java
@@ -2,16 +2,16 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         java.util.Scanner sc = new java.util.Scanner(System.in);
-        int a = intScan.nextInt();
+        int a = sc.nextInt();
         int a1 = a % 4;
         int a2 = a % 100;
         int a3 = a % 400;
 
         if ((a1 == 0) && (a2 > 0)) {
-            System.out.println('1');
+            System.out.println("true");
         } else if (a3 == 0) {
-            System.out.println('1');
+            System.out.println("true");
         } else
-            System.out.println('0');
+            System.out.println("false");
     }
 }
