a7972d7614bf54904ed3d3552272e469f5c358f8
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-01-03 09:22:04 +0900

    Title: 두 사람, Time: 137ms, Memory: 10MB, Status: Wrong Answer - CodeTree

---
+++ 240103/두 사람/two-person.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a1 = sc.nextInt();
+        String a2 = sc.next();
+        int b1 = sc.nextInt();
+        String b2 = sc.next();
+        if (a1 >= 19 || b1 >= 19) {
+            if (a2.equals("M") || b2.equals("M")) {
+                System.out.println("1");
+            } else {
+                System.out.println("0");
+            }
+        } else {
+            System.out.println("0");
+        }
+    }
+}
