72650a2b9952fc6bb526692bfc40f195c61ec541
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-01-07 02:58:10 +0900

    Title: 특정 숫자 도달하기 2, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240107/특정 숫자 도달하기 2/reaching-specific-number-2.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        for( ;a>=1;a--){
+            System.out.printf("%d ", a);
+        }
+    }
+}
