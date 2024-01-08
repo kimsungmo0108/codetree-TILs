a91bad28eabd8e4d4d1eec8c720e50992896b485
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-01-08 14:20:35 +0900

    Title: 홀수만 출력 2, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240108/홀수만 출력 2/output-only-odd-2.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for (; a>=b ; a--){
+            if(a%2==1){
+                System.out.printf("%d ", a);
+            }
+        }
+    }
+}
