4d6d0d2fd6d1d7221137f416c7b98d1b43b58926
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-01-11 15:27:07 +0900

    Title: a/b 출력, Time: 139ms, Memory: 10MB, Status: Passed - CodeTree

--- 240111/a/b 출력/a-divide-b.java
+++ 240111/a/b 출력/a-divide-b.java
@@ -2,8 +2,14 @@ public class Main {
     public static void main(String[] args) {
         // 여기에 코드를 작성해주세요.
         java.util.Scanner sc = new java.util.Scanner(System.in);
-        double a = sc.nextDouble();
-        double b = sc.nextDouble();
-        System.out.printf("%.20f", a/b);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        System.out.printf("%d.", a/b);
+        a %= b;
+        for(int i = 0; i<20; i++){
+            a *= 10;
+            System.out.print(a/b);
+            a %= b;
+        }
     }
 }
