dfcfdf9c2d523d5782c7fc0e0cef51d3dd86f857
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-01-23 14:10:40 +0900

    Title: 20대의 평균나이, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

---
+++ 240123/20대의 평균나이/average-age-of-20.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int age = 0;
+        int total = 0;
+        int c = 0;
+        while(true){
+            age = sc.nextInt();
+            if(age>=30){
+                break;
+            }else{
+                total += age;
+                c++;
+            }
+        }
+        System.out.printf("%.2f", ((float)total/c));
+    }
+}
