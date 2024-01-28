3d0904e0a74526fa7be9282fec86be824fad7d54
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-01-28 21:17:52 +0900

    Title: 정사각형 두 개 출력, Time: 136ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240128/정사각형 두 개 출력/output-two-rectangles.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
+        for(int k=0; k<2; k++){
+            for(int i =1; i<=n; i++){
+                for(int j=1; j<=n; j++){
+                    System.out.print("*");
+                }
+                System.out.print("\n");
+            }
+            System.out.print("\n");
+        }
+    }
+}
