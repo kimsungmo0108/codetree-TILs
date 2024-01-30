fd0e6e29430e338c53e0e4f5ae2b60827655f189
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-01-30 16:33:29 +0900

    Title: 별표 출력하기, Time: 138ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240130/별표 출력하기/print-star.java
@@ -0,0 +1,20 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
+        for(int i=1; i<=n; i++){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+
+        for(int i=n-1;i>0;i--){
+            for(int j=1; j<=i; j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
