3123336d3702bddf25d2f1c840cf5f0adbebbb8b
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-02-07 18:13:01 +0900

    Title: 행과 열, Time: 155ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240207/행과 열/row-column.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        for(int i=1; i<=a; i++){
+            for(int j=1; j<=b; j++){
+                System.out.printf("%d ", i*j);
+            }
+            System.out.println();
+        }
+    }
+}
