2e5e226158aeda396bfe484c0264f87294c004e0
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-02-20 16:54:14 +0900

    Title: 학점 계산기, Time: 152ms, Memory: 11MB, Status: Wrong Answer - CodeTree

---
+++ 240220/학점 계산기/credit-calculator.java
@@ -0,0 +1,21 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        int n = sc.nextInt();
+        float sum = 0;
+        float avg = 0;
+        for(int i=0; i<n; i++){
+            sum += sc.nextFloat();
+        }
+        avg = sum/n;
+        System.out.printf("%.1f\n", avg);
+        if(avg>=4.0){
+            System.out.println("Perfect");
+        }else if(avg>=3.0){
+            System.out.println("Good");
+        }else if(avg<3.0){
+            System.out.println("Poor");
+        }
+    }
+}
