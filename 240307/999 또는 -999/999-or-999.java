63c6b8cd0fc9a144844553ea6c728952cedbb729
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-03-07 09:20:47 +0900

    Title: 999 또는 -999, Time: 163ms, Memory: 10MB, Status: Passed - CodeTree

--- 240307/999 또는 -999/999-or-999.java
+++ 240307/999 또는 -999/999-or-999.java
@@ -3,8 +3,8 @@ public class Main {
         // 여기에 코드를 작성해주세요.
         java.util.Scanner sc = new java.util.Scanner(System.in);
         int a = 0;
-        int max = 0;
-        int min = 0;
+        int max = -999;
+        int min = 999;
         for(;;){
             a = sc.nextInt();
             if(a==999 || a==-999){
