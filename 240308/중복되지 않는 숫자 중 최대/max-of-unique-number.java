b6337bf590e18ac3c96cbecc1b8d17cb4434badf
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-03-08 14:24:02 +0900

    Title: 중복되지 않는 숫자 중 최대, Time: 218ms, Memory: 13MB, Status: Passed - CodeTree

--- 240308/중복되지 않는 숫자 중 최대/max-of-unique-number.java
+++ 240308/중복되지 않는 숫자 중 최대/max-of-unique-number.java
@@ -1,27 +1,40 @@
-public class Main {
-    public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        int n =sc.nextInt();
-        int c = -1;
-        int[] nums = new int[n];
-        int max = -1;
-        for(int i=0; i<n; i++){
-            nums[i] = sc.nextInt();
-        }
+import java.util.Scanner;
 
-        for(int i=0; i<n; i++){
-            if(max == nums[i]){
-                c=-1;
-            }else{
-                max = max>nums[i]?max:nums[i];
-                c++;
+public class Main {
+    public static final int MAX_N = 1000;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n;
+        int[] nums = new int[MAX_N];
+
+        // 입력
+        n = sc.nextInt();
+        for(int i = 0; i < n; i++)
+            nums[i] = sc.nextInt();
+
+        // 최댓값 찾기
+        int max = -1;
+        for(int i = 0; i < n; i++) {
+            int currNum = nums[i];
+            // 최대가 될 수 있는 후보입니다.
+            if(max < currNum) {
+                // 갱신할 수 있는지 확인하기 위해 이 숫자의 등장 빈도를 셉니다.
+                int count = 0;
+                for(int j = 0; j < n; j++) {
+                    if(nums[j] == currNum) {
+                        count++;
+                    }
+                }
+                // 이 숫자가 배열에서 유일할때만 갱신합니다.
+                if(count == 1) {
+                    max = currNum;
+                }
             }
         }
-        if(c == -1){
-            System.out.println(c);
-        }else{
-            System.out.println(max);
-        }
+
+        // 최댓값과 최솟값을 출력합니다.
+        System.out.print(max);
     }
 }
