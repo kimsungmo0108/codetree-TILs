7702e967bed7fe03d831209d0297bf5805c181f3
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-03-28 12:39:36 +0900

    Title: 문자 개수 세기, Time: 127ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240328/문자 개수 세기/count-char.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        String str = sc.nextLine();
+        char c = sc.next().charAt(0);
+        int count=0;
+        for(int i=0; i<str.length(); i++){
+            if(str.charAt(i) == c){
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
+}
