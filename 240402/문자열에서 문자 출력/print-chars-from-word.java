cca14bda32105d4a2092bb7dbead480d12c998b6
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-04-02 09:18:18 +0900

    Title: 문자열에서 문자 출력, Time: 121ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240402/문자열에서 문자 출력/print-chars-from-word.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        String str = sc.next();
+        for(int i=0; i<str.length(); i++){
+            System.out.printf("%s\n", str.charAt(i));
+        }
+    }
+}
