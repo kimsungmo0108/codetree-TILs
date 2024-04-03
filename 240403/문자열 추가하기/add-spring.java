a99950825f6bcd1cc80179045884c5f6c9229350
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-04-03 11:36:06 +0900

    Title: 문자열 추가하기, Time: 154ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240403/문자열 추가하기/add-spring.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        String str = sc.next();
+        String h = "Hello";
+        System.out.println(str+h);
+    }
+}
