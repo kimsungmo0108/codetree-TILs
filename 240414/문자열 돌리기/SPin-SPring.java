c6e7d7c1a5f6c1a2def8195f203cfb16b3835403
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-04-14 20:07:37 +0900

    Title: 문자열 돌리기, Time: 236ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240414/문자열 돌리기/SPin-SPring.java
@@ -0,0 +1,13 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        String str = sc.next();
+        int len = str.length();
+        System.out.println(str);
+        for(int i=0; i<len; i++){
+            str = str.substring(len-1) + str.substring(0, len-1);
+            System.out.println(str);
+        }
+    }
+}
