261c374c2c06df03e5995570444645fcd604a996
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-04-14 20:54:56 +0900

    Title: 문자열 한 칸씩 밀어내며 뒤집기, Time: 320ms, Memory: 19MB, Status: Passed - CodeTree

--- 240414/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
+++ 240414/문자열 한 칸씩 밀어내며 뒤집기/shift-reverse-string.java
@@ -1,30 +1,60 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // 여기에 코드를 작성해주세요.
-        java.util.Scanner sc = new java.util.Scanner(System.in);
-        String str = sc.next();
-        int n = sc.nextInt();
-        // int[] num = new int[n];
-        int num = 0;
-        String str2 = new String();
-        // for(int i=0; i<n; i++){
-        //     num[i] = sc.nextInt();
-        // }
-        for(int i=0; i<n; i++){
-            num = sc.nextInt();
-           if(num == 1){
-                str = str.substring(1, str.length()) + str.substring(0, 1);
-                System.out.println(str);
-            }else if(num == 2){
-                str = str.substring(str.length()-1) + str.substring(0, str.length()-1);
-                System.out.println(str);
-            }else if(num == 3){
-                for(int j = str.length()-1; j>=0; j--){
-                    str2 += String.valueOf(str.charAt(j));
+        Scanner sc = new Scanner(System.in);
+        
+        // 문자열을 입력받습니다.
+        String inputStr = sc.next();
+        int qNum = sc.nextInt();
+
+        int strSize = inputStr.length();
+        
+        int qType;
+        for(int i = 0; i < qNum; i++) {
+            qType = sc.nextInt();
+            if(qType == 1) {
+                // step1: 가장 앞의 문자를 저장한 뒤,
+                // step2: 문자열을 앞부터 순회하며 문자를 한 칸씩 앞으로 당기고
+                // step3: 문자열의 제일 뒤에 가장 앞에 있던 문자를 넣어줍니다.
+                char[] arr = inputStr.toCharArray();
+
+                char front = arr[0];              // step1
+                for(int j = 1; j < strSize; j++) // step2
+                    arr[j - 1] = arr[j];
+                arr[strSize - 1] = front;        // step3
+
+                inputStr = new String(arr);
+                System.out.println(inputStr);
+            }
+            else if(qType == 2) {
+                // step1: 가장 뒤의 문자를 저장한 뒤,
+                // step2: 문자열의 뒤부터 순회하며 문자를 한 칸씩 뒤로 밀어주고
+                // step3: 문자열의 제일 앞에 가장 뒤에 있던 문자를 넣어줍니다. 
+                char[] arr = inputStr.toCharArray();
+
+                char back = arr[strSize - 1];          // step1
+                for(int j = strSize - 1; j >= 1; j--)  // step2
+                    arr[j] = arr[j - 1];    
+                arr[0] = back;					        // step3
+
+                inputStr = new String(arr);
+                System.out.println(inputStr);
+            }
+            else if(qType == 3) {
+                // 문자열의 앞부터 순회하며 좌우 대칭 위치에 있는 문자와 swap해줍니다. 
+                // 단, 문자열의 절반만 순회해줍니다.
+                char[] arr = inputStr.toCharArray();
+
+                char temp;
+                for(int j = 0; j < strSize / 2; j++) {
+                    temp = arr[j];
+                    arr[j] = arr[strSize - j - 1];
+                    arr[strSize - j - 1] = temp;
                 }
-                str = str2;
-                System.out.println(str);
-                str2 = "";
+
+                inputStr = new String(arr);
+                System.out.println(inputStr);
             }
         }
     }
