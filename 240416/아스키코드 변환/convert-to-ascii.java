fa1b172a890bb7eb1361b12f3df901af4c1ac2e5
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-04-16 09:11:40 +0900

    Title: 아스키코드 변환, Time: 145ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240416/아스키코드 변환/convert-to-ascii.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        char c = sc.next().charAt(0);
+        int n = sc.nextInt();
+        System.out.printf("%d %s", (int)c, (char)n);
+    }
+}
