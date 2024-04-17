34b07b6a29f04a5c5803371dd210854c88f906ce
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-04-17 09:11:10 +0900

    Title: 그 다음 알파벳, Time: 134ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240417/그 다음 알파벳/next-alphabet.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        char c = sc.next().charAt(0);
+        if(c == 'z'){
+            c = (char)96;
+        }
+        System.out.printf("%s", (char)(c+1));
+    }
+}
