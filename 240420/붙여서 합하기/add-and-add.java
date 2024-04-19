4d62a4cdf3c891c66e3be121b9a62a5da578a41c
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-04-20 00:56:56 +0900

    Title: 붙여서 합하기, Time: 184ms, Memory: 10MB, Status: Passed - CodeTree

---
+++ 240420/붙여서 합하기/add-and-add.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        // 여기에 코드를 작성해주세요.
+        java.util.Scanner sc = new java.util.Scanner(System.in);
+        String str1 = sc.next();
+        String str2 = sc.next();
+        System.out.printf("%d", Integer.parseInt(str1+str2)+Integer.parseInt(str2+str1));
+    }
+}
