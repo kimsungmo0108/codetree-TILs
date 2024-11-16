e55ad12c682b00e1e4bea2328ef807cfd74808fb
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-11-16 20:11:06 +0900

    Title: 알파벳에 따른 평가, Time: 43ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241116/알파벳에 따른 평가/evaluation-by-alphabet.py
@@ -0,0 +1,14 @@
+grade = input().strip()
+
+if grade == 'S':
+    print("Superior")
+elif grade == 'A':
+    print("Excellent")
+elif grade == 'B':
+    print("Good")
+elif grade == 'C':
+    print("Usually")
+elif grade == 'D':
+    print("Effort")
+else:
+    print("Failure")
