aab9e45ac3b7f8572945da0cbb79c0db29f89ddd
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-11-19 20:40:44 +0900

    Title: 특정 조건 두 정수 비교, Time: 41ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241119/특정 조건 두 정수 비교/specific-comparison-of-two-natural-numbers.py
@@ -0,0 +1,13 @@
+a, b = input().split();
+a = int(a);
+b = int(b);
+
+if(a < b):
+    print("1", end=" ");
+else:
+    print("0", end=" ");
+
+if(a == b):
+    print("1", end=" ");
+else:
+    print("0", end=" ");
