e2dce42b64ac7f009334a4a2efdf00bfde61f2f9
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-11-20 22:28:01 +0900

    Title: 점수 비교, Time: 39ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241120/점수 비교/score-comparison.py
@@ -0,0 +1,12 @@
+a1, a2 = input().split();
+a1 = int(a1);
+a2 = int(a2);
+
+b1, b2 = input().split();
+b1 = int(b1);
+b2 = int(b2);
+
+if a1 > b1 and a2 > b2:
+    print("1");
+else:
+    print("0");
