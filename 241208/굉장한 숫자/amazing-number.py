eb7c4e3ae48980f07f687a765774ee50dc893cf3
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-12-08 21:06:05 +0900

    Title: 굉장한 숫자, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

--- 241208/굉장한 숫자/amazing-number.py
+++ 241208/굉장한 숫자/amazing-number.py
@@ -3,6 +3,10 @@ n = int(input());
 if n%2 == 1:
     if n%3 == 0:
         print("true");
+    else:
+        print("false");
 elif n%2 == 0:
     if n%5 == 0:
         print("true");
+    else:
+        print("false");
