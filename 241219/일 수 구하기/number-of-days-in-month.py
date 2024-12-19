0ded29aa4f5207c9feb189c25ae03ed1e5b2b2c5
kimsungmo0108 <150896012+kimsungmo0108@users.noreply.github.com>
2024-12-19 16:58:20 +0900

    Title: 일 수 구하기, Time: 44ms, Memory: 16MB, Status: Passed - CodeTree

---
+++ 241219/일 수 구하기/number-of-days-in-month.py
@@ -0,0 +1,14 @@
+n = int(input())
+
+if n == 2:
+	print("28")
+elif n <= 7:
+    if n % 2 == 1:
+        print("31")
+    else:
+        print("30")
+else:
+    if n % 2 == 0:
+        print("31")
+    else:
+        print("30")
